00464f1b3696bc06704342bdd57ac01a0b090252
hmejdini <166559056+hmejdini@users.noreply.github.com>
2026-05-25 20:53:02 +0200

    Delete app/core/app/core/config.py

--- app/core/app/core/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "default_token_per_testim")
-
-settings = Settings()
